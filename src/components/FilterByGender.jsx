b7c7339be96def48fb21d5bf0fcd1811bd65bc25
iewaw <izabelawawrzyniak90@gmail.com>
2024-02-18 12:05:10 +0100

    improving media queries

--- src/components/FilterByGender.jsx
+++ src/components/FilterByGender.jsx
@@ -10,35 +10,41 @@ function FilterByGender({ selectedGender, handleFilter }) {
     <div className="filterGender">
       <h2>Selecciona el género:</h2>
       <div className="filterRadio">
-        <input
-          type="radio"
-          id="female"
-          name="gender"
-          value="Female"
-          checked={selectedGender === "Female"}
-          onChange={handleInputGender}
-        />
-        <label htmlFor="female">Mujer ♀️</label>
+        <div>
+          <input
+            type="radio"
+            id="female"
+            name="gender"
+            value="Female"
+            checked={selectedGender === "Female"}
+            onChange={handleInputGender}
+          />
+          <label htmlFor="female"> Mujer ♀️</label>
+        </div>
 
-        <input
-          type="radio"
-          id="male"
-          name="gender"
-          value="Male"
-          checked={selectedGender === "Male"}
-          onChange={handleInputGender}
-        />
-        <label htmlFor="male">Hombre ♂️</label>
+        <div>
+          <input
+            type="radio"
+            id="male"
+            name="gender"
+            value="Male"
+            checked={selectedGender === "Male"}
+            onChange={handleInputGender}
+          />
+          <label htmlFor="male"> Hombre ♂️</label>
+        </div>
 
-        <input
-          type="radio"
-          id="all"
-          name="gender"
-          value="All"
-          checked={selectedGender === "All"}
-          onChange={handleInputGender}
-        />
-        <label htmlFor="all">Tod@s ✨</label>
+        <div>
+          <input
+            type="radio"
+            id="all"
+            name="gender"
+            value="All"
+            checked={selectedGender === "All"}
+            onChange={handleInputGender}
+          />
+          <label htmlFor="all"> Tod@s ✨</label>
+        </div>
       </div>
     </div>
   );
